19d85d37ed4e285b13f831d32e79f80738ffb714
Ryan Vagle <rsvagle@gmail.com>
2019-11-29 12:40:17 -0600

    Add files via upload

---
+++ todolist_react/precache-manifest.6557e5616556a224c7fded3531f74318.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12139b06ab4c172d8368ed6a63d75d7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e471bf2578d14c5a0e9d",
+    "url": "/static/css/main.721eae11.chunk.css"
+  },
+  {
+    "revision": "91ab5c37c517e328ef12",
+    "url": "/static/js/2.870a1d58.chunk.js"
+  },
+  {
+    "revision": "e471bf2578d14c5a0e9d",
+    "url": "/static/js/main.d8612d1c.chunk.js"
+  },
+  {
+    "revision": "76967e75227249d33e7d",
+    "url": "/static/js/runtime-main.d1382be6.js"
+  }
+]);
